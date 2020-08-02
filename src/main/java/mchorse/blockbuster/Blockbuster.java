a6c8be4770073fd39aa45175da105a46d32c2334
Eliot Lash <fadookie@gmail.com>
2020-08-02 09:33:37 -0700

    delete some unused code

--- src/main/java/mchorse/blockbuster/Blockbuster.java
+++ src/main/java/mchorse/blockbuster/Blockbuster.java
@@ -6,6 +6,4 @@ public class Blockbuster {
     public static String MOD_ID = "particleman";
 
     public static Value<Boolean> particleSorting = new Value(false);
-    public static Value<Integer> chromaSkyColor = new Value(0);
-    public static Value<Boolean> chromaSky = new Value(false);
 }
--- src/main/java/mchorse/blockbuster/client/RenderingHandler.java
+++ src/main/java/mchorse/blockbuster/client/RenderingHandler.java
@@ -1,37 +1,21 @@
 package mchorse.blockbuster.client;
 
 import mchorse.blockbuster.Blockbuster;
-//import mchorse.blockbuster.ClientProxy;
-//import mchorse.blockbuster.client.gui.GuiRecordingOverlay;
 import mchorse.blockbuster.client.particles.emitter.BedrockEmitter;
-//import mchorse.blockbuster.client.render.tileentity.TileEntityGunItemStackRenderer;
-//import mchorse.blockbuster.client.render.tileentity.TileEntityModelItemStackRenderer;
-import mchorse.blockbuster.client.textures.GifTexture;
-//import mchorse.blockbuster.recording.RecordRecorder;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GLSync;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Rendering handler
@@ -42,61 +26,10 @@ import java.util.Map;
 @SideOnly(Side.CLIENT)
 public class RenderingHandler
 {
-//    private static TileEntityModelItemStackRenderer model = new TileEntityModelItemStackRenderer();
-//    private static TileEntityGunItemStackRenderer gun = new TileEntityGunItemStackRenderer();
-
-    /**
-     * GIFs which should be updated
-     */
-    public static Map<ResourceLocation, GifTexture> gifs = new HashMap<ResourceLocation, GifTexture>();
-
     /**
      * Bedrock particle emitters
      */
     private static final List<BedrockEmitter> emitters = new ArrayList<>();
-
-//    private GuiRecordingOverlay overlay;
-
-    /**
-     * Register GIF
-     */
-    public static void registerGif(ResourceLocation rl, GifTexture texture)
-    {
-        GifTexture old = gifs.remove(rl);
-
-        if (old != null)
-        {
-            old.deleteGlTexture();
-        }
-
-        gifs.put(rl, texture);
-    }
-
-    /**
-     * Render green sky, this is getting invoked from the ASM patched
-     * code in {@link RenderGlobal}
-     */
-    public static void renderGreenSky()
-    {
-        int color = Blockbuster.chromaSkyColor.get();
-        float skyR = (color >> 16 & 0xff) / 255F;
-        float skyG = (color >> 8 & 0xff) / 255F;
-        float skyB = (color & 0xff) / 255F;
-        float skyA = (color >> 24 & 0xff) / 255F;
-
-        GlStateManager.clearColor(skyR, skyG, skyB, skyA);
-        GlStateManager.clear(GL11.GL_COLOR_BUFFER_BIT);
-        GL11.glDisable(GL11.GL_FOG);
-    }
-
-    /**
-     * Check whether it's time to render green sky, this is getting
-     * invoked from ASM patched code in {@link RenderGlobal}
-     */
-    public static boolean isGreenSky()
-    {
-        return Blockbuster.chromaSky.get();
-    }
 
     /**
      * Render lit particles (call by ASM, but not used for anything yet...
@@ -204,71 +137,5 @@ public class RenderingHandler
     public RenderingHandler(/*GuiRecordingOverlay overlay*/)
     {
 //        this.overlay = overlay;
-    }
-
-    /**
-     * Fixes lightmap for TEISR
-     */
-    @SubscribeEvent
-    public void onHUDRender(RenderGameOverlayEvent.Pre event)
-    {
-        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL)
-        {
-            Minecraft.getMinecraft().entityRenderer.enableLightmap();
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
-            Minecraft.getMinecraft().entityRenderer.disableLightmap();
-        }
-    }
-
-    /**
-     * Renders recording overlay during HUD rendering
-     */
-    @SubscribeEvent
-    public void onHUDRender(RenderGameOverlayEvent.Post event)
-    {
-        ScaledResolution resolution = event.getResolution();
-
-//        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL)
-//        {
-//            this.overlay.draw(resolution.getScaledWidth(), resolution.getScaledHeight());
-//        }
-    }
-
-    /**
-     * Add Blockbuster debug strings, such as length of client records and
-     * recording information, to the debug overlay.
-     */
-    @SubscribeEvent
-    public void onHUDRender(RenderGameOverlayEvent.Text event)
-    {
-        if (!Minecraft.getMinecraft().gameSettings.showDebugInfo)
-        {
-            return;
-        }
-
-        List<String> list = event.getLeft();
-
-        list.add("");
-//        list.add(ClientProxy.manager.records.size() + " client records");
-
-//        RecordRecorder recorder = ClientProxy.manager.recorders.get(Minecraft.getMinecraft().player);
-
-//        if (recorder != null)
-//        {
-//            list.add("Recording frame " + recorder.tick + " (delay: " + recorder.delay + ")");
-//        }
-    }
-
-    /**
-     * On render last world event, this bad boy will tick all of the GIF
-     * textures which were registered
-     */
-    @SubscribeEvent
-    public void onRenderLast(RenderWorldLastEvent event)
-    {
-        for (GifTexture texture : gifs.values())
-        {
-            texture.tick();
-        }
     }
 }
