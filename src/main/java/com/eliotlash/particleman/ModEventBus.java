7d8fa5343236618fbf76da4d2e724a0ac1b25fbf
Eliot Lash <fadookie@gmail.com>
2020-08-07 10:03:51 -0700

    comment out example code

--- src/main/java/com/eliotlash/particleman/ModEventBus.java
+++ src/main/java/com/eliotlash/particleman/ModEventBus.java
@@ -23,8 +23,11 @@ public class ModEventBus
     {
         if (event.phase == TickEvent.Phase.START && event.player == Minecraft.getMinecraft().player)
         {
+            // Example usage of particle system:
+            /*
             if (scheme == null)
             {
+                // This was just for quick testing, you can also load particles from your resources using Manager.loadScheme
                 scheme = BedrockScheme.parse("{\"format_version\":\"1.10.0\",\"particle_effect\":{\"description\":{\"identifier\":\"snowstorm:rainbow\",\"basic_render_parameters\":{\"material\":\"particles_alpha\",\"texture\":\"examplemod:textures/default_particles.png\"}},\"curves\":{\"variable.rainbow\":{\"type\":\"catmull_rom\",\"input\":\"variable.particle_random_2\",\"horizontal_range\":1,\"nodes\":[1,0,1,1.18]},\"variable.psize\":{\"type\":\"catmull_rom\",\"input\":\"variable.particle_age\",\"horizontal_range\":\"variable.particle_lifetime\",\"nodes\":[0,0,1,0,0]}},\"components\":{\"minecraft:emitter_initialization\":{\"creation_expression\":\"variable.radius = 2.5;test = 1;\"},\"minecraft:emitter_rate_steady\":{\"spawn_rate\":250,\"max_particles\":500},\"minecraft:emitter_lifetime_looping\":{\"active_time\":4},\"minecraft:emitter_shape_point\":{\"offset\":[\"math.cos(variable.emitter_age * 90) * (2.5-variable.particle_random_2)\",\"math.sin(variable.emitter_age * 90) * (2.5-variable.particle_random_2)\",0]},\"minecraft:particle_lifetime_expression\":{\"max_lifetime\":1.5},\"minecraft:particle_initial_speed\":0,\"minecraft:particle_motion_dynamic\":{\"linear_acceleration\":[0,1,0]},\"minecraft:particle_appearance_billboard\":{\"size\":[\"0.12 * variable.psize\",\"0.12 * variable.psize\"],\"facing_camera_mode\":\"rotate_xyz\",\"uv\":{\"texture_width\":128,\"texture_height\":128,\"uv\":[32,88],\"uv_size\":[8,8]}},\"minecraft:particle_appearance_tinting\":{\"color\":{\"interpolant\":\"variable.rainbow\",\"gradient\":{\"0.0\":\"#d71c1c\",\"0.16\":\"#ffdf00\",\"0.33\":\"#08ff00\",\"0.5\":\"#00ffff\",\"0.67\":\"#0000ff\",\"0.83\":\"#ff00ff\",\"1.0\":\"#e21111\"}}}}}}");
             }
 
@@ -40,8 +43,12 @@ public class ModEventBus
                 pointEmitter.setScheme(scheme);
             }
 
+            // Attach emitter to entity
             Manager.updateEmitter(playerEmitter, event.player);
+
+            // Attach emitter to point in space
             Manager.updateEmitter(pointEmitter, new Vec3d(0, 56, 0), event.player.world);
+             */
         }
     }
 }
